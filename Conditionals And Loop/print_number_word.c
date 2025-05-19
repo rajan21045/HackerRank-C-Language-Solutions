303d017670522f9fadc11a1e5d3a1f6e7c388857
Rajan Poudel <raajan.works@gmail.com>
2025-05-19 20:17:52 +0545

    Update print_number_word.c

--- Conditionals And Loop/print_number_word.c
+++ Conditionals And Loop/print_number_word.c
@@ -1,8 +1,10 @@
 /*
- * Write a program in C that takes a positive integer n as input
- * and prints its lowercase English word representation 
- * if it lies between 1 and 9 (both inclusive).
- * If the number is greater than 9, print: Greater than 9
+    Write a program in C that takes a positive integer n as input 
+    and prints its lowercase English word representation.
+    
+ 1. if it lies between 1 and 9 (both inclusive).
+ 2. If the number is greater than 9, print: Greater than 9.
+
  */
 
 #include <stdio.h>  // Include standard input-output header
@@ -17,30 +19,39 @@ int main() {
     if (n == 1) {
         printf("one");
     }
+    
     else if (n == 2) {
         printf("two");
     }
+    
     else if (n == 3) {
         printf("three");
     }
+    
     else if (n == 4) {
         printf("four");
     }
+    
     else if (n == 5) {
         printf("five");
     }
+    
     else if (n == 6) {
         printf("six");
     }
+    
     else if (n == 7) {
         printf("seven");
     }
+    
     else if (n == 8) {
         printf("eight");
     }
+    
     else if (n == 9) {
         printf("nine");
     }
+    
     else {
         // If n is greater than 9, print this message
         printf("Greater than 9");
