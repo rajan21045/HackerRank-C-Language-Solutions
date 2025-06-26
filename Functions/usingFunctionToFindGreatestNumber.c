b2201a240a4181195a8f9189cfcb30b6c1fc9727
Rajan Poudel <raajan.works@gmail.com>
2025-06-26 15:44:59 +0545

    Add files via upload

---
+++ Functions/usingFunctionToFindGreatestNumber.c
@@ -0,0 +1,58 @@
+/*
+
+Task
+Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.
+
+Note
+There is not built in max function in C. Code that will be reused is often put in a separate function, e.g. int max(x, y) that returns the greater of the two values.
+
+*/
+
+#include <stdio.h>
+
+int max_of_four(int a,int b, int c,int d){
+    int max;
+    
+    max = a;
+    if(b> max){
+        max = b;
+    }
+    if(c>max){
+        max = c;
+    }
+    if(d> max){
+        max =d;
+    }
+    return max;
+}
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+/*
+Objective
+In this challenge, you will learn simple usage of functions in C. Functions are a bunch of statements grouped together. A function is provided with zero or more arguments, and it executes the statements on it. Based on the return type, it either returns nothing (void) or something.
+
+Input Format
+Input will contain four integers - a, b, c, d, one on each line.
+
+Output Format
+Print the greatest of the four integers.
+Note: I/O will be automatically handled.
+
+
+Sample Input
+3
+4
+6
+5
+
+Sample Output
+6
+
+*/
